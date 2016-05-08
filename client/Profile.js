37c2a065b2a72a0bffec6d7a8520f83338ccb931
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2016-05-08 00:20:21 -0400

    Retrieve images from collection and render on profile

--- client/Profile.js
+++ client/Profile.js
@@ -1,11 +1,17 @@
+import { Mongo } from 'meteor/mongo';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Images } from '../imports/collections.js';
+import './Profile.html';
+
 
 Template.Profile.helpers({
   // profile: function(){
   //   return 0;
   // },
 
-  find_images() {
-      return users.find({});
+  find_users() {
+      return Meteor.users.find({});
     }
 
 });
